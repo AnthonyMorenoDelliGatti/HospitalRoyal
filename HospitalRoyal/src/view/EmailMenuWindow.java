7b13a5580088f455d2d3678944efc488d5797964
nrosahinojosa.sanjose@alumnado.fundacionloyola.net <60214715+Nicorosa@users.noreply.github.com>
2020-12-17 09:31:24 +0100

    Actualizacion clase emailmenuwindow

--- HospitalRoyal/src/view/EmailMenuWindow.java
+++ HospitalRoyal/src/view/EmailMenuWindow.java
@@ -44,23 +44,13 @@ public class EmailMenuWindow {
 	private JPanel emailBox;
 	private JTextField textField;
 
-	/**
-	 * Launch the application.
-	 */
-
-	/**
-	 * Create the application.
-	 */
 	public EmailMenuWindow() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
-		
+
 		frame.setResizable(false);
 		frame.getContentPane().setForeground(WHITE);
 		frame.setBounds(100, 100, 429, 530);
