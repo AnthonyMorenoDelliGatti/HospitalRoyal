11daaf0e18ac60f1b223d1594423b7ae9282bd22
Fran <Fran@DESKTOP-1495OQO>
2020-12-18 14:11:26 +0100

    implementation of delete of non-empty folders

--- HospitalRoyal/src/controller/ListenerEliminar.java
+++ HospitalRoyal/src/controller/ListenerEliminar.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 import model.Archivo;
 import view.VistaArchivos;
@@ -39,7 +40,9 @@ public class ListenerEliminar implements ActionListener {
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, "");
 
 		if (eleccion == JOptionPane.YES_OPTION) {
-			// Agregar el proceso de eliminacion
+			try {
+				String originalPath = client.printWorkingDirectory();
+			// proceso de eliminacion
 			if (archivo.getIsCarpeta() == 0) {
 				try {
 					client.deleteFile(archivo.getDireccion());				
@@ -48,10 +51,60 @@ public class ListenerEliminar implements ActionListener {
 					e1.printStackTrace();
 				}
 			} else {
-				
+				try {
+					if(!client.removeDirectory(archivo.getDireccion())) {
+						String previousPath = client.printWorkingDirectory();
+						client.changeWorkingDirectory(archivo.getDireccion());
+						delete(client.listFiles());
+						client.changeWorkingDirectory(previousPath);
+						client.removeDirectory(archivo.getDireccion());
+					}			
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 			// se actualiza la vista
+			try {
+				client.changeWorkingDirectory(originalPath);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			method.cargarDatosLista(archivos, client, view, explorer);
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			};
+		}
+	}
+
+	private void delete(FTPFile[] listFiles) {
+		if(listFiles.length != 0) {
+		for(FTPFile file : listFiles) {
+			if (!file.isDirectory()) {
+				try {
+					client.deleteFile(client.printWorkingDirectory()+"/"+file.getName());				
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			} else {
+				try {
+					if(!client.removeDirectory(client.printWorkingDirectory()+"/"+file.getName())) {
+						String previousPath = client.printWorkingDirectory();
+						System.out.println(client.printWorkingDirectory()+"/"+file.getName());
+						client.changeWorkingDirectory(client.printWorkingDirectory()+"/"+file.getName());
+						delete(client.listFiles());
+						client.changeWorkingDirectory(previousPath);
+						client.removeDirectory(client.printWorkingDirectory()+"/"+file.getName());
+					}			
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
 		}
 	}
 
