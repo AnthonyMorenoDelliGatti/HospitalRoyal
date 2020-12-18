033fa200291c58a913c82b19b1af8fe5ba3a2a04
Gonzalo <gruizdemiermora.sanjose@alumnado.fundacionloyola.net>
2020-12-18 13:33:39 +0100

    Mejora de Splash

--- HospitalRoyal/src/view/Splash.java
+++ HospitalRoyal/src/view/Splash.java
@@ -1,11 +1,17 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.border.EmptyBorder;
 
 import controller.Client;
@@ -14,19 +20,28 @@ public class Splash extends JFrame implements Runnable{
 
 	Thread splash = new Thread();
 	private JPanel contentPane;
+	JProgressBar progressBar = new JProgressBar();
 	
 	public Splash() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 587, 668);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		this.setUndecorated(true);
-		JLabel label = new JLabel();
-		label.setIcon(new ImageIcon(Splash.class.getResource("..\\iconos\\Logo.PNG")));
-		contentPane.add(label, BorderLayout.CENTER);
+		JLabel icono = new JLabel();
+		icono.setIcon(new ImageIcon(Splash.class.getResource("..\\iconos\\Logo.PNG")));
+		contentPane.add(icono,BorderLayout.CENTER);
+		
+		JLabel gif = new JLabel();
+		gif.setIcon(new ImageIcon(Splash.class.getResource("..\\iconos\\cargando.gif")));
+		contentPane.add(gif,BorderLayout.EAST);
+		
+		
+		contentPane.setBackground(new Color(255, 209, 247));
+		pack();
 	}
+	
 	@Override
 	public void run() {
 		try {
