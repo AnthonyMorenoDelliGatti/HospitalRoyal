149e6130c7580ddac7f56b6f479348ba0db9e589
nrosahinojosa.sanjose@alumnado.fundacionloyola.net <60214715+Nicorosa@users.noreply.github.com>
2021-01-07 09:15:02 +0100

    Implementacion log para cada accion

--- HospitalRoyal/src/view/VistaArchivos.java
+++ HospitalRoyal/src/view/VistaArchivos.java
@@ -33,8 +33,9 @@ public class VistaArchivos {
 	VistaPrincipal vista;
 	ArrayList<Archivo> archivos;
 	String user;
-	
-	public VistaArchivos(FTPClient client, ArrayList<Archivo> archivos, Methods method, VistaPrincipal vista, String user) {
+
+	public VistaArchivos(FTPClient client, ArrayList<Archivo> archivos, Methods method, VistaPrincipal vista,
+			String user) {
 		this.client = client;
 		this.method = method;
 		this.vista = vista;
@@ -70,7 +71,7 @@ public class VistaArchivos {
 			JLabel l = obtenerIcono(i);
 			panel.add(l);
 			JTextField nombre = generarNombre(panel, i);
-			panel.add(new JLabel(""+i.getUltFechaModificacion()));
+			panel.add(new JLabel("" + i.getUltFechaModificacion()));
 			panel.addMouseListener(new ListenerArchivo(panel, i));
 			JPopupMenu menu = generarMenu(nombre, i);
 			panel.setComponentPopupMenu(menu);
@@ -82,15 +83,15 @@ public class VistaArchivos {
 
 	private JPopupMenu generarMenu(JTextField nombre, Archivo archivo) {
 		JPopupMenu menu = new JPopupMenu();
-		
+
 		item = new JMenuItem("Cambiar nombre");
 		item.addActionListener(new ListenerBotonModificarNombre(nombre));
 		menu.add(item);
 		JMenuItem item2 = new JMenuItem("Descargar");
-		item2.addActionListener(new ListenerDescargar(archivo.getDireccion(), archivo.getNombre(), client, method));
+		item2.addActionListener(new ListenerDescargar(archivo.getDireccion(), archivo.getNombre(), client, method, user));
 		menu.add(item2);
 		item3 = new JMenuItem("Eliminar");
-		item3.addActionListener(new ListenerEliminar(archivo,archivos,client,method,vista,this, user));
+		item3.addActionListener(new ListenerEliminar(archivo, archivos, client, method, vista, this, user));
 		menu.add(item3);
 		return menu;
 	}
