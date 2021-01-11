a4ea229f2900aacc39fcc2862a301c8a07e9591a
nrosahinojosa.sanjose@alumnado.fundacionloyola.net <60214715+Nicorosa@users.noreply.github.com>
2021-01-11 13:14:15 +0100

    creacion de la rama

--- HospitalRoyal/src/controller/Client.java
+++ HospitalRoyal/src/controller/Client.java
@@ -205,37 +205,37 @@ public class Client {
 		StartMenu(adminUser, client);
 	}
 
-	private void enviarConGMail(String destinatario, String asunto, String cuerpo) {
-		// Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el
-		// remitente también.
-		String remitente = "hospitalroyalcontact"; // Para la dirección nomcuenta@gmail.com
-		String clave = "Hospitalroyal2021";
-
-		Properties props = System.getProperties();
-		props.put("mail.smtp.host", "smtp.gmail.com"); // El servidor SMTP de Google
-		props.put("mail.smtp.user", remitente);
-		props.put("mail.smtp.clave", clave); // La clave de la cuenta
-		props.put("mail.smtp.auth", "true"); // Usar autenticación mediante usuario y clave
-		props.put("mail.smtp.starttls.enable", "true"); // Para conectar de manera segura al servidor SMTP
-		props.put("mail.smtp.port", "587"); // El puerto SMTP seguro de Google
-
-		Session session = Session.getDefaultInstance(props);
-		MimeMessage message = new MimeMessage(session);
-
-		try {
-			message.setFrom(new InternetAddress(remitente));
-			message.addRecipients(Message.RecipientType.TO, destinatario); // Se podran añadir varios de la misma
-																			// manera
-			message.setSubject(asunto);
-			message.setText(cuerpo);
-			Transport transport = session.getTransport("smtp");
-			transport.connect("smtp.gmail.com", remitente, clave);
-			transport.sendMessage(message, message.getAllRecipients());
-			transport.close();
-		} catch (MessagingException me) {
-			me.printStackTrace(); // Si se produce un error
-		}
-	}
+//	private void enviarConGMail(String destinatario, String asunto, String cuerpo) {
+//		// Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el
+//		// remitente también.
+//		String remitente = "hospitalroyalcontact"; // Para la dirección nomcuenta@gmail.com
+//		String clave = "Hospitalroyal2021";
+//
+//		Properties props = System.getProperties();
+//		props.put("mail.smtp.host", "smtp.gmail.com"); // El servidor SMTP de Google
+//		props.put("mail.smtp.user", remitente);
+//		props.put("mail.smtp.clave", clave); // La clave de la cuenta
+//		props.put("mail.smtp.auth", "true"); // Usar autenticación mediante usuario y clave
+//		props.put("mail.smtp.starttls.enable", "true"); // Para conectar de manera segura al servidor SMTP
+//		props.put("mail.smtp.port", "587"); // El puerto SMTP seguro de Google
+//
+//		Session session = Session.getDefaultInstance(props);
+//		MimeMessage message = new MimeMessage(session);
+//
+//		try {
+//			message.setFrom(new InternetAddress(remitente));
+//			message.addRecipients(Message.RecipientType.TO, destinatario); // Se podran añadir varios de la misma
+//																			// manera
+//			message.setSubject(asunto);
+//			message.setText(cuerpo);
+//			Transport transport = session.getTransport("smtp");
+//			transport.connect("smtp.gmail.com", remitente, clave);
+//			transport.sendMessage(message, message.getAllRecipients());
+//			transport.close();
+//		} catch (MessagingException me) {
+//			me.printStackTrace(); // Si se produce un error
+//		}
+//	}
 
 	private void StartMenu(boolean adminUser, FTPClient client) {
 		vStartMenu = new StartMenuView();
@@ -323,7 +323,7 @@ public class Client {
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					
-					enviarConGMail("user.hospitalroyal@gmail.com", "PRUEBA", "FIOHEDAOUFHAF"); //PRUEBA
+//					enviarConGMail("user.hospitalroyal@gmail.com", "PRUEBA", "FIOHEDAOUFHAF"); //PRUEBA
 					
 					vStartMenu.setVisible(false);
 					emailwindow = new EmailMenuWindow(user, vStartMenu);
