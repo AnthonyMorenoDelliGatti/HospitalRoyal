31a166c3e885ec9c00c6e7fd1f52a38bc1853547
AnthonyMorenoDelliGatti <60214459+AnthonyMorenoDelliGatti@users.noreply.github.com>
2021-01-13 23:07:54 +0100

    Arreglo para socket ya iniciado y BD cerrada

--- HospitalRoyal/src/servidor/controller/ThreadServer.java
+++ HospitalRoyal/src/servidor/controller/ThreadServer.java
@@ -10,6 +10,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 import client.controller.Methods;
 import servidor.view.ServerView;
 
@@ -18,13 +20,14 @@ public class ThreadServer extends Thread {
 	DataInputStream inputStream;
 	DataOutputStream outputStream;
 	Hospital hospital;
-	Methods method;
+	static Methods method;
 	ServerView viewServer;
+	static boolean connectedDB;
 
 	public ThreadServer(Socket client, Hospital hospital, ServerView viewServer, Methods method) throws IOException {
 		this.client = client;
 		this.hospital = hospital;
-		this.viewServer=viewServer;
+		this.viewServer = viewServer;
 		outputStream = new DataOutputStream(client.getOutputStream());
 		inputStream = new DataInputStream(client.getInputStream());
 		this.method = method;
@@ -36,71 +39,77 @@ public class ThreadServer extends Thread {
 		try {
 			outputStream = new DataOutputStream(client.getOutputStream());
 			inputStream = new DataInputStream(client.getInputStream());
-			int loop=1;
-			while(loop==1) {
+			int loop = 1;
+			while (loop == 1) {
 				String user = inputStream.readUTF();
 				String password = inputStream.readUTF();
 				if (checkUser(user, password)) {
 					outputStream.writeUTF("CORRECT USER OR PASSWORD");
 					// USUARIO Y CONTRASE�A CORRECTOS
 					method.log(user, 1, "Log in");
-					if(checkPermissions(user)) {
+					if (checkPermissions(user)) {
 						outputStream.writeUTF("normalUser");
-					}else {
+					} else {
 						outputStream.writeUTF("adminUser");
 					}
 					outputStream.writeUTF(selectEmailsUser(user));
-					while(loop==1) {
+					while (loop == 1) {
 						String action = inputStream.readUTF();
 						int actionId = Integer.parseInt(action);
 						switch (actionId) {
 						case 2:
-							//Logout
+							// Logout
 							method.log(user, 2, "Logout");
-							viewServer.getArea().append("\n"+client.toString()+" logged out");
-							loop=0;
+							viewServer.getArea().append("\n" + client.toString() + " logged out");
+							loop = 0;
 							break;
 						case 3:
-							//Borrar archivo
+							// Borrar archivo
 							String fileName = inputStream.readUTF();
 							method.log(user, 3, "Delete file: " + fileName);
-							viewServer.getArea().append("\n"+client.toString()+" deleted file: " + fileName);
+							viewServer.getArea().append("\n" + client.toString() + " deleted file: " + fileName);
 							break;
 						case 4:
-							//Subir archivo
+							// Subir archivo
 							String route = inputStream.readUTF();
 							method.log(user, 4, "Upload: " + route);
-							viewServer.getArea().append("\n"+client.toString()+" uploaded: " + route);
+							viewServer.getArea().append("\n" + client.toString() + " uploaded: " + route);
 							break;
 						case 5:
-							//Crear carpeta
+							// Crear carpeta
 							String folder = inputStream.readUTF();
 							method.log(user, 5, "Created directory: " + folder);
-							viewServer.getArea().append("\n"+client.toString()+" created directory: " + folder);
+							viewServer.getArea().append("\n" + client.toString() + " created directory: " + folder);
 							break;
 						case 6:
-							//Borrar carpeta
+							// Borrar carpeta
 							String folderName = inputStream.readUTF();
 							method.log(user, 6, " has deleted the folder " + folderName);
-							viewServer.getArea().append("\n"+client.toString()+" deleted the folder " + folderName);
+							viewServer.getArea().append("\n" + client.toString() + " deleted the folder " + folderName);
 							break;
 						case 7:
-							//Cambiar nombre
+							// Cambiar nombre
 							String name = inputStream.readUTF();
 							String newName = inputStream.readUTF();
 							method.log(user, 7, " renamed file: " + name + " to: " + newName);
-							viewServer.getArea().append("\n"+client.toString()+" renamed file: " + name + " to: " + newName);
+							viewServer.getArea()
+									.append("\n" + client.toString() + " renamed file: " + name + " to: " + newName);
 							break;
 						case 8:
-							//Descargar archivo
+							// Descargar archivo
 							String fileNameDownload = inputStream.readUTF();
 							method.log(user, 8, " downloaded file: " + fileNameDownload);
-							viewServer.getArea().append("\n"+client.toString()+" downloaded file: " + fileNameDownload);
+							viewServer.getArea()
+									.append("\n" + client.toString() + " downloaded file: " + fileNameDownload);
 							break;
 						}
 					}
 				} else {
-					outputStream.writeUTF("INCORRECT USER OR PASSWORD");
+					if (!connectedDB) {
+						outputStream.writeUTF("DB NOT CONNECTED");
+					} else {
+						outputStream.writeUTF("INCORRECT USER OR PASSWORD");
+					}
 				}
 			}
 		} catch (IOException e) {
@@ -109,51 +118,49 @@ public class ThreadServer extends Thread {
 	}
 
 	private String selectEmailsUser(String user) {
-		String email="";
+		String email = "";
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/hospital_royal", "root", "");
-			Statement statement = connection.createStatement();
-			String sql = "SELECT correo FROM usuario WHERE usuario LIKE '"+user+"'";
+			Statement statement = method.DBConnection();
+			String sql = "SELECT correo FROM usuario WHERE usuario LIKE '" + user + "'";
 			ResultSet resul;
 			resul = statement.executeQuery(sql);
 			while (resul.next()) {
-				email=resul.getString(1); //send the email of the user
+				email = resul.getString(1); // send the email of the user
 			}
+			connectedDB = true;
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "The database is not available", "FAILED TO LINK WITH DATA BASE",
+					JOptionPane.WARNING_MESSAGE);
+			connectedDB = false;
 		}
 		return email;
 	}
 
 	private boolean checkPermissions(String user) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/hospital_royal", "root", "");
-			Statement statement = connection.createStatement();
+			Statement statement = method.DBConnection();
 			String sql = "SELECT * FROM usuario";
 			ResultSet resul;
 			resul = statement.executeQuery(sql);
 			while (resul.next()) {
 				if (user.equals(resul.getString(1))) {
-					if(resul.getInt(3)==1) {
+					if (resul.getInt(3) == 1) {
 						return true;
 					}
 				}
 			}
+			connectedDB = true;
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "The database is not available", "FAILED TO LINK WITH DATA BASE",
+					JOptionPane.WARNING_MESSAGE);
+			connectedDB = false;
 		}
 		return false;
 	}
 
 	private static boolean checkUser(String user, String password) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/hospital_royal", "root", "");
-			Statement statement = connection.createStatement();
+			Statement statement = method.DBConnection();
 			String sql = "SELECT * FROM usuario";
 			ResultSet resul;
 			resul = statement.executeQuery(sql);
@@ -164,9 +171,11 @@ public class ThreadServer extends Thread {
 					}
 				}
 			}
+			connectedDB = true;
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "The database is not available", "FAILED TO LINK WITH DATA BASE",
+					JOptionPane.WARNING_MESSAGE);
+			connectedDB = false;
 		}
 		return false;
 	}
