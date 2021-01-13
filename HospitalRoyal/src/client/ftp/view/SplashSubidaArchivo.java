02b8aebed66b137090b80e99ca1eb82d58f3fdd7
AnthonyMorenoDelliGatti <60214459+AnthonyMorenoDelliGatti@users.noreply.github.com>
2021-01-13 11:20:32 +0100

    splash descarga implementado

--- HospitalRoyal/src/client/ftp/view/SplashSubidaArchivo.java
+++ HospitalRoyal/src/client/ftp/view/SplashSubidaArchivo.java
@@ -2,6 +2,9 @@ package client.ftp.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -9,45 +12,63 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.Timer;
 import javax.swing.border.Border;
 
 import client.controller.Client;
 
-public class SplashSubidaArchivo extends JFrame{
+public class SplashSubidaArchivo extends JFrame {
 
 	Thread splash = new Thread();
 	private JProgressBar barra;
 	JPanel panel = new JPanel();
 	JButton btn;
-	public SplashSubidaArchivo() {
+
+	public SplashSubidaArchivo(FTPWindow v, String tipe) {
 		panel.setLayout(new BorderLayout(0, 0));
 		panel.setBackground(Color.white);
 		this.setUndecorated(true);
 		btn = new JButton("");
-		btn.setIcon(new ImageIcon(("iconos\\descargaSplash.png")));
+		if (tipe.equals("download")) {
+			btn.setIcon(new ImageIcon(("iconos\\descargaSplash.png")));
+		} else if (tipe.equals("upload")) {
+			btn.setIcon(new ImageIcon(("iconos\\descargaSplash.png")));
+		}
 		btn.setFocusPainted(false);
 		Border emptyBorder = BorderFactory.createEmptyBorder();
 		btn.setBorder(emptyBorder);
 		btn.setBackground(Color.white);
-		panel.add(btn,BorderLayout.CENTER);
-		
+		panel.add(btn, BorderLayout.CENTER);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		barra=new JProgressBar(0,100);
-		barra.setForeground(Color.green);
-		panel.add(barra,BorderLayout.SOUTH);
-		
+		JProgressBar progressbar = new JProgressBar();
+		progressbar.setStringPainted(true);
+		progressbar.setValue(0);
+		progressbar.setForeground(Color.green);
+		int timerDelay = 10;
+		new Timer(timerDelay, new ActionListener() {
+			private int index = 0;
+			private int maxIndex = 100;
+
+			public void actionPerformed(ActionEvent e) {
+				if (index < maxIndex) {
+					progressbar.setValue(index);
+					index++;
+				} else {
+					progressbar.setValue(maxIndex);
+					((javax.swing.Timer) e.getSource()).stop();
+					v.setEnabled(true);
+					dispose();
+				}
+			}
+		}).start();
+
+		progressbar.setValue(progressbar.getMinimum());
+		panel.add(progressbar, BorderLayout.SOUTH);
+
 		setContentPane(panel);
 		pack();
 		setVisible(true);
-		cargar();
 	}
-	private void cargar() {
-		for(int i=0;i<=100;i++) {
-			barra.setValue(i);
-			if(i==100) {
-				this.dispose();	
-			}
-		}
-	}
-	
+
 }
